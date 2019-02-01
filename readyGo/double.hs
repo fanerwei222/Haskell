a286868ae62f7022fe89d3e38de5ac18df320329
樊伟 <fanerwei222@163.com>
2019-02-01 09:30:46 +0800

    Update double.hs

--- readyGo/double.hs
+++ readyGo/double.hs
@@ -1,11 +1,13 @@
 
-
+定义一个函数，参数是x
 doubleMe x = x + x
 
+定义一个函数，参数是x 和 y
 doubleUs x y = 2*x + 2*y
 
 doubleUs x y = doubleMe x + doubleMe y
 
+if else强制需要
 doubleSmallNumber x = if x > 100
                       then x
                       else x*2
