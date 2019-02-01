2e17fae02941e11031c977d9436fcd6582c1b14b
樊伟 <fanerwei222@163.com>
2019-02-01 09:28:45 +0800

    Update double.hs

--- readyGo/double.hs
+++ readyGo/double.hs
@@ -1,1 +1,13 @@
+
+
 doubleMe x = x + x
+
+doubleUs x y = 2*x + 2*y
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x*2
+                      
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
